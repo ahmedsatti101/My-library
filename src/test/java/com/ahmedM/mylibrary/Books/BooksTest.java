12498acdef391ed1714722d120a2ec0b6237d4f1
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-06-08 12:28:49 +0100

    Work in progress

--- src/test/java/com/ahmedM/mylibrary/Books/BooksTest.java
+++ src/test/java/com/ahmedM/mylibrary/Books/BooksTest.java
@@ -6,6 +6,8 @@ import com.ahmedM.mylibrary.Genres.GenreRepository;
 import com.ahmedM.mylibrary.Genres.Genres;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +17,6 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Iterator;
-
-import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
@@ -57,49 +56,22 @@ public class BooksTest {
     @Test
     public void getAllBooks() throws Exception {
         String json = GetBooksFromJson.GetBooksFromJson("src/main/resources/data/books.json");
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(json);
-        JsonNode booksNode = rootNode.path("books");
+        JSONObject obj = new JSONObject(json);
+        JSONArray arr = obj.getJSONArray("books");
+        Authors author = new Authors();
+        Genres genre = new Genres();
+        Books book = new Books();
 
-        Iterator<JsonNode> iterator = booksNode.elements();
-
-        while (iterator.hasNext()) {
-            Authors author = new Authors();
-            Genres genre = new Genres();
-            Books book = new Books();
-
-            System.out.println(iterator.next());
-            JsonNode bookNode = iterator.next();
-            author.setAuthorId(bookNode.path("authorId").asInt());
-//
-//            genre.setGenreId(bookNode.path("genreId").asInt());
-//            genre.setName("Genre Name " + genre.getGenreId());
-//
-//            book.setTitle(bookNode.path("title").asText());
-//            book.setAuthor(author);
-//            book.setGenre(genre);
-//            book.setCover(bookNode.path("cover").asText());
-//            book.setRead(bookNode.path("isRead").asBoolean());
-//            book.setDescription(bookNode.path("description").asText());
-//
-//            booksRepository.save(book);
+        for (int i = 0; i < arr.length(); i++) {
+            author.setAuthorId(arr.getJSONObject(i).getInt("genreId"));
+            genre.setGenreId(arr.getJSONObject(i).getInt("authorId"));
+            book.setTitle(arr.getJSONObject(i).getString("title"));
+            book.setCover(arr.getJSONObject(i).getString("cover"));
+            book.setRead(arr.getJSONObject(i).getBoolean("isRead"));
+            book.setDescription(arr.getJSONObject(i).getString("description"));
         }
 
-//        Authors author = new Authors();
-//        Genres genre = new Genres();
-
-//        author.setName("Ahmed Mohamed");
-//        genre.setName("Horror");
-//
-//        Books book = new Books();
-//        book.setTitle("Life");
-//        book.setAuthor(author);
-//        book.setGenre(genre);
-//        book.setCover("");
-//        book.setRead(false);
-//        book.setDescription("Sucks");
-//
-//        booksRepository.save(book);
+            booksRepository.save(book);
 
         mockMvc.perform(get(api))
                 .andExpect(status().isOk())

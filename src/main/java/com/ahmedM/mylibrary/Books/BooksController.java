9d17865739a63ff85ea16dbc9eb95b073edc287f
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-06-12 10:47:19 +0100

    added GET /api/books/:id to get a single book by its id

--- src/main/java/com/ahmedM/mylibrary/Books/BooksController.java
+++ src/main/java/com/ahmedM/mylibrary/Books/BooksController.java
@@ -2,12 +2,11 @@ package com.ahmedM.mylibrary.Books;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -19,5 +18,14 @@ public class BooksController {
     @ResponseStatus(HttpStatus.OK)
     public List<Books> getAllBooks() {
         return booksService.findAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Books> getBookById(@PathVariable int id) {
+        if (booksService.findBookById(id).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found");
+        }
+        return booksService.findBookById(id);
     }
 }
--- src/test/java/com/ahmedM/mylibrary/Books/BooksTest.java
+++ src/test/java/com/ahmedM/mylibrary/Books/BooksTest.java
@@ -1,20 +1,17 @@
 package com.ahmedM.mylibrary.Books;
 
-import org.json.JSONArray;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.json.*;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -50,5 +47,42 @@ public class BooksTest {
 
         assertNotNull(content);
         assertEquals(40, array.length());
+    }
+
+    @Test
+    public void returnNotFoundIfEndpointIsWrong() throws Exception {
+        mockMvc.perform(get("/api/book"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getBookById() throws Exception {
+        MvcResult result = mockMvc.perform(get(api + "/{id}", 7))
+                .andExpect(status().isOk())
+                .andReturn();
+        String response = result.getResponse().getContentAsString();
+        JSONObject object = new JSONObject(response);
+
+        assertNotNull(response);
+        assertEquals(7, object.getInt("bookId"));
+        assertEquals("Dystopian Dreams", object.getString("title"));
+        assertEquals(7, object.getInt("authorId"));
+        assertEquals(7, object.getInt("genreId"));
+        assertEquals("", object.getString("cover"));
+        assertFalse(object.getBoolean("read"));
+        assertEquals("A dark vision of the future.", object.getString("description"));
+    }
+
+    @Test
+    public void throwNotFoundIfIdNotFound() throws Exception {
+        mockMvc.perform(get(api + "/{id}", 200))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Book not found"));
+    }
+
+    @Test
+    public void throwBadRequestIfIdIsNotANumber() throws Exception {
+        mockMvc.perform(get(api + "/{id}", "banana"))
+                .andExpect(status().isBadRequest());
     }
 }
